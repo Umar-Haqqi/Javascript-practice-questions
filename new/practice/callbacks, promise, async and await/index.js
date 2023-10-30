cd63a14e03b3c08089665b1bc59bfd8295da23cc
Umar-Haqqi <umer1562001@gmail.com>
2023-10-31 02:16:57 +0500

    handle data displaying error with callback

--- new/practice/callbacks, promise, async and await/index.js
+++ new/practice/callbacks, promise, async and await/index.js
@@ -11,22 +11,44 @@ const userData = [
         name: "Umar",
         profession: "software engineer"
     },
-    {
-        name: "Ali",
-        profession: "Digital marketing"
-    },
+
 ]
 
-const displayList = document.querySelector(".list")
+const displaydata = document.querySelector(".dataTable")
 
 const getData = () => {
     setTimeout(() => {
         let output = ''
         userData.forEach((data, index) => {
             index = index + 1
-            output += `<li class = "myli">${index}: ${data.name}</li>`
+            // output += `<li class = "myli">${index}: ${data.name}</li>`
+
+            output += `
+            <table>
+                <tr>
+                    <td>${index}<td>
+                    <td>${data.name}<td>
+                    <td>${data.profession}<td>
+                </tr>
+            </table>`
         })
-        displayList.innerHTML = output
+        displaydata.innerHTML = output
     }, 1000)
 }
 getData();
+
+
+// it will not display the new data if its timeout is greater then getData function
+// to aviod this problem we will use callback
+
+function createUserData(addData, callback) {
+    setTimeout(() => {
+        userData.push(addData)
+
+        callback() // calling getdata function here
+        
+    }, 3000);
+}
+// createUserData({ name: "Ali", profession: "Digital marketing" })
+createUserData({ name: "Ali", profession: "Digital marketing" }, getData) // passing getData reference here
+createUserData({ name: "Ahmad", profession: "Digital marketing" }, getData)
