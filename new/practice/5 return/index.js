70e1ca7a4e63b8209355e95be2cd43b822ae2be6
Umar-Haqqi <umer1562001@gmail.com>
2023-11-15 14:04:53 +0500

    return value in javascript

---
+++ new/practice/5 return/index.js
@@ -0,0 +1,8 @@
+function myFunc(a, b) {
+    const add = a + b;
+
+    return add;
+    // if there is no return statement then bydefault is undefined
+}
+const result = myFunc(12, 12);
+console.log(result);
