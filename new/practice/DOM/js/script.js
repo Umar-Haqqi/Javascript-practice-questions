5f8291d40db4c364965c638243eceee3f7980eea
Umar-Haqqi <umer1562001@gmail.com>
2023-11-21 09:49:13 +0500

    Create and Append new list item

--- new/practice/DOM/js/script.js
+++ new/practice/DOM/js/script.js
@@ -1,5 +1,21 @@
 const elem = document.querySelector('.para')
 const p = document.createElement('h1');
+
+p.className = "heading";
+p.id = "headingID";
+
 const text = document.createTextNode("hello hello");
 p.appendChild(text)
 elem.appendChild(p)
+
+
+// how to read an element
+const main_heading = document.querySelector('#main-heading')
+console.log(main_heading.textContent);
+
+
+// add a new list item
+const list = document.querySelector('.list')
+const li = document.createElement('li')
+li.textContent = "new 4"
+list.appendChild(li)
